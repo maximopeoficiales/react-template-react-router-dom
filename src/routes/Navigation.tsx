58f1f857a7e2096270ac928c67dbb1b6c27a16f5
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-02-10 01:20:18 -0500

    ADD: rutas dinamicas segun un archivo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -5,8 +5,10 @@ import {
   NavLink,
   Navigate
 } from 'react-router-dom';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../lazyload/pages';
 
 import logo from '../logo.svg';
+import { routes } from './routes';
 
 export const Navigation = () => {
   return (
@@ -15,27 +17,21 @@ export const Navigation = () => {
         <nav>
           <img src={logo} alt="React Logo" />
           <ul>
-            <li>
-              <NavLink to="/home" className={({ isActive }) => isActive ? "nav-active" : ""}>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" className={({ isActive }) => isActive ? "nav-active" : ""}>About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/users" className={({ isActive }) => isActive ? "nav-active" : ""}>Users</NavLink>
-            </li>
+            {routes.map(e =>
+              <li key={e.to}>
+                <NavLink to={e.to} className={({ isActive }) => isActive ? "nav-active" : ""}>{e.name}</NavLink>
+              </li>
+            )}
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Routes>
-
-          <Route path="about" element={<h1>About</h1>} />
-          <Route path="users" element={<h1>Users</h1>} />
-          <Route path="home" element={<h1>Home</h1>} />
-          <Route path="/*" element={<Navigate to="/home" replace />} />
-
+          {routes.map(e =>
+            <Route path={e.path} element={<e.component />} />
+          )}
+          <Route path="/*" element={<Navigate to="/lazy1" replace />} />
         </Routes>
       </div>
     </Router>
